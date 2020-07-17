982b6ee120d23f47251307b9ad050154ca2ea842
Breno <breno.nunesgalvao@protonmail.ch>
2020-07-17 20:42:26 -0300

    patch notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :store_history
-
-  private
-
-  def store_history
-    return unless request.get?
-    return if request.xhr?
-
-    session[:history] ||= []
-    session[:history].delete_at(0) if session[:history].size >= 10
-    session[:history] << request.url unless session[:history].include? request.url
-  end
 end
